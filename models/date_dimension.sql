d652988cf0e10ba286b102db5bd5592e85a8103d
Aniket Nayak <aniketnayak1999@gmail.com>
2026-01-27 13:45:34 +0000

    CORRECTION IN DAY_TPYE IN DATE_DIMENSION

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,7 +5,7 @@ with cte as (
     HOUR(to_timestamp(STARTED_AT)) AS HOUR_STARTED_AT,
 
     CASE 
-    WHEN DAYNAME(to_timestamp(STARTED_AT)) IN ('sat','sun')
+    WHEN DAYNAME(to_timestamp(STARTED_AT)) IN ('Sat','Sun')
     then 'WEEKEND'
     ELSE 'BUSINESSDAY'
     END AS DAY_TYPE,
